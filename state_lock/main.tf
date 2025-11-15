d43ff9684ce89809ccbcf7226c7eb3ec91df44e6
UbiquitousChris <15385371+UbiquitousChris@users.noreply.github.com>
2025-11-15 14:27:33 -0700

    Created a backend file to include across multiple terraform modules.

--- state_lock/main.tf
+++ state_lock/main.tf
@@ -1,11 +1,6 @@
 terraform {
     backend "s3" {
-        bucket          = "terraform-up-and-running-state-ubiquitouschris"
         key             = "global/s3/terraform.tfstate"
-        region          = "us-east-2"
-
-        dynamodb_table  = "terraform-up-and-running-locks"
-        encrypt         = true
     }
 }
 
