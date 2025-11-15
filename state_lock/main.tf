1107fa7c70894ca272b64b0aa9286f21f1e68fba
UbiquitousChris <15385371+UbiquitousChris@users.noreply.github.com>
2025-11-15 13:35:22 -0700

    Added resource to enable server-side encryption by default

--- state_lock/main.tf
+++ state_lock/main.tf
@@ -11,9 +11,22 @@ resource "aws_s3_bucket" "terraform_state" {
     }
 }
 
+# Enabled versioning so you can see the full revision history of your
+# state files
 resource "aws_s3_bucket_versioning" "enabled" {
     bucket = aws_s3_bucket.terraform_state.id
     versioning_configuration {
         status = "Enabled"
+    }
+}
+
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+    bucket = aws_s3_bucket.terraform_state.id
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
     }
 }
